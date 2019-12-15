2f1ae1a7cac95e48cd17c43e765add3facb034a4
JUNE-29 <jiyuni529@gmail.com>
2019-12-15 17:49:01 +0900

    v06 추가

--- bitcamp-java/src/main/java/com/eomcs/ok/Hello3.java
+++ bitcamp-java/src/main/java/com/eomcs/ok/Hello3.java
@@ -2,6 +2,56 @@ package com.eomcs.ok;
 
 public class Hello3 {
   public static void main(String[] args) {
-    System.out.println("ok/Hello3!");
-  }
+
+    /*for(int i = 0; i < 5; i++)
+    {
+      for(int j=0; j<=i; j++)
+        System.out.print("*");
+      System.out.println();
+    }*/
+    
+    /*for(int i = 5; i > 0; i--){
+      for(int j=0; j<i; j++)
+        System.out.print("*");
+    System.out.println();
+    }*/
+    
+    /*for(int i=0; i<5; i++) {
+      for(int j=4-i; j>0; j--)
+        System.out.print(" ");
+      
+      for(int k=0; k<=i; k++)
+        System.out.print("*");
+      System.out.println();
+    }*/
+    
+    // 구구단 출력 
+//    for(int j=2; j<10; j++)
+//      for(int i=1; i<10; i++)
+//      System.out.printf("%d * %d = %d\n", j, i, j*i);
+     
+    
+    // 피보나치 수열의 30번째 항을 구하는 프로그램을 작성하시오.
+    // 피보나치 수열 0 1 1 2 3 5 8 13 21 34 ... 0 1 1(0+1)2(1+1)3(2+1)5(2+3)...
+    
+//    int num1 = 0, num2 = 1, num3;
+//    for(int i=3; i<33; i++) {
+//      num3 = num1 + num2;
+//      System.out.printf("피보나치 수열의 %d번째 값: %d\n", i, num3);
+//      num1 = num2;
+//      num2 = num3;
+//    }
+    // int[] arr = {1,2,3,4,5}; 2 하고 4 자리 바꾸기
+    //Swap
+//    int[] arr ={1,2,3,4,5};
+//    int tmp;
+//    tmp = arr[1];
+//    arr[1] = arr[3];
+//    arr[3] = tmp;
+//    
+
+   
+
+    
+ }
 }
