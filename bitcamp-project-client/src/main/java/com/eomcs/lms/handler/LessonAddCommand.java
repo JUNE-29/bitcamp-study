450c528fb199421d387eaaee62b50338b20444a8
JUNE-29 <jiyuni529@gmail.com>
2020-02-04 20:37:30 +0900

    2월4일 수업정리

---
+++ bitcamp-project-client/src/main/java/com/eomcs/lms/handler/LessonAddCommand.java
@@ -0,0 +1,52 @@
+package com.eomcs.lms.handler;
+
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import com.eomcs.lms.domain.Lesson;
+import com.eomcs.util.Prompt;
+
+public class LessonAddCommand implements Command {
+
+  ObjectOutputStream out;
+  ObjectInputStream in;
+
+  public Prompt prompt;
+
+  public LessonAddCommand(ObjectOutputStream out, ObjectInputStream in, Prompt prompt) {
+    this.out = out;
+    this.in = in;
+    this.prompt = prompt;
+  }
+
+
+  @Override
+  public void execute() {
+    Lesson lesson = new Lesson();
+
+    lesson.setNo(prompt.inputInt("번호? "));
+    lesson.setTitle(prompt.inputString("수업명? "));
+    lesson.setDescription(prompt.inputString("설명? "));
+    lesson.setStartDate(prompt.inputDate("시작일? "));
+    lesson.setEndDate(prompt.inputDate("종료일? "));
+    lesson.setTotalHours(prompt.inputInt("총 수업시간? "));
+    lesson.setDayHours(prompt.inputInt("일 수업시간? "));
+
+    try {
+      out.writeUTF("/lesson/add");
+      out.writeObject(lesson);
+      out.flush();
+
+      String response = in.readUTF();
+      if (response.equals("FAIL")) {
+        System.out.println(in.readUTF()); // 서버의 메시지(왜 실패했는지)를 읽는다.
+        return;
+      }
+      System.out.println("저장하였습니다.");
+    } catch (Exception e) {
+      System.out.println("통신 오류 발생!");
+    }
+  }
+}
+
+
