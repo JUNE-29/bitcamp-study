0d9ca035bf650b1ad68dd03c811beea705e7680a
JUNE-29 <jiyuni529@gmail.com>
2020-02-07 11:21:42 +0900

    2월 7일 수업정리

---
+++ bitcamp-project-server/src/main/java/com/eomcs/lms/dao/proxy/MemberDaoProxy.java
@@ -0,0 +1,90 @@
+package com.eomcs.lms.dao.proxy;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.List;
+import com.eomcs.lms.dao.MemberDao;
+import com.eomcs.lms.domain.Member;
+
+// 프록시 객체는 항상 작업 객체와 동일한 인터페이스를 구현해야 한다.
+// => 마치 자신이 작업 객체인양 보이기 위함이다.
+// => MemberDao 작업 객체를 대행할njknfkjsdnksjdnfsdkj
+
+public class MemberDaoProxy implements MemberDao {
+
+  ObjectInputStream in;
+  ObjectOutputStream out;
+
+  public MemberDaoProxy(ObjectInputStream in, ObjectOutputStream out) {
+    this.in = in;
+    this.out = out;
+  }
+
+  @Override
+  public int insert(Member member) throws Exception {
+    out.writeUTF("/member/add");
+    out.writeObject(member);
+    out.flush();
+
+    String response = in.readUTF();
+    if (response.equals("FAIL")) {
+      throw new Exception(in.readUTF());
+      // 이유를 읽고 Exception으로 해서 던진다. 누구에게..?
+    }
+    return 1;
+  }
+
+  @SuppressWarnings("unchecked")
+  @Override
+  public List<Member> findAll() throws Exception {
+    out.writeUTF("/member/list"); // 서버에 요청!
+
+    out.flush();
+
+    String response = in.readUTF();
+    if (response.equals("FAIL")) {
+      throw new Exception(in.readUTF());
+    }
+    return (List<Member>) in.readObject();
+  }
+
+  @Override
+  public Member findByNo(int no) throws Exception {
+    out.writeUTF("/member/detail");
+    out.writeInt(no);
+    out.flush();
+
+    String response = in.readUTF();
+    if (response.equals("FAIL")) {
+      throw new Exception(in.readUTF());
+    }
+    return (Member) in.readObject();
+  }
+
+  @Override
+  public int update(Member member) throws Exception {
+    out.writeUTF("/member/update");
+    out.writeObject(member);
+    out.flush();
+
+    String response = in.readUTF();
+    if (response.equals("FAIL")) {
+      throw new Exception(in.readUTF());
+    }
+    return 1; // 한 개 업데이트 했다.
+  }
+
+  @Override
+  public int delete(int no) throws Exception {
+    out.writeUTF("/member/delete");
+    out.writeInt(no);
+    out.flush();
+
+    String response = in.readUTF();
+    if (response.equals("FAIL")) {
+      throw new Exception(in.readUTF());
+    }
+    return 1;
+  }
+
+}
