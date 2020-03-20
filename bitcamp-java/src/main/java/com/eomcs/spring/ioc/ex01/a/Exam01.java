4813bf9ff3ab88841928acffcfe999b390649820
JUNE-29 <jiyuni529@gmail.com>
2020-03-20 17:50:39 +0900

    3월20일 수업정리 - Spring IoC

--- bitcamp-java/src/main/java/com/eomcs/spring/ioc/ex01/a/Exam01.java
+++ bitcamp-java/src/main/java/com/eomcs/spring/ioc/ex01/a/Exam01.java
@@ -16,14 +16,16 @@ public class Exam01 {
     // Spring IoC 컨테이너
     // => spring.io 사이트에서 제공하는 프레임워크이다.
     // => 프로젝트에 Spring IoC 컨테이너 포함하기
-    // mvnrepository.com 에서 spring-context 로 라이브러리를 검색한다.
-    // build.gradle 에 의존 라이브러리 정보를 추가한다.
-    // '$ gradle eclipse'를 실행하여 라이브러리 파일을 다운로드 받고 이클립스 설정 파일을 갱신한다.
-    // 이클립스에서 프로젝트 정보를 갱신한다.
+    // mvnrepository.com 또는 search.maven.org에서
+    // spring-context 로 라이브러리를 검색한다.
+    // => build.gradle 에 의존 라이브러리 정보를 추가한다.
+    // => '$ gradle eclipse'를 실행하여 라이브러리 파일을 다운로드 받고
+    // 이클립스 설정 파일을 갱신한다.
+    // => 이클립스에서 프로젝트 정보를 갱신한다.
     //
     // ApplicationContext 인터페이스
     // => 스프링 IoC 컨테이너의 사용 규칙을 정의한 인터페이스이다.
-    // => 모든 스프링 IoC 컨테이너는 이 규칙에 따라 IoC 컨테이너가 정의되어 있다.
+    // => 모든 스프링 IoC 컨테이너는 이 규칙에 따라 정의되어 있다.
     //
     // ApplicationContext 구현체(implements, 인터페이스를 구현한 클래스 또는 그 클래스의 인스턴스)의 종류
     // 1) XML 파일에서 설정 정보를 읽어들이는 IoC 컨테이너
@@ -41,9 +43,8 @@ public class Exam01 {
     // 물론 JVM이 클래스 파일을 찾을 JVM의 기본 경로($JAVA_HOME/lib)를 가장 먼저 뒤진다.
     // => 설정 파일 경로를 지정할 때 자바 패키지 경로를 지정한다.
     // 파일 경로이기 때문에 패키지와 패키지 사이에는 . 대신에 /를 사용해야 한다.
-    ApplicationContext iocContainer1 =
-        new ClassPathXmlApplicationContext("com/eomcs/spring/ioc/ex01/a/application-context.xml");
-
+    ApplicationContext iocContainer = new ClassPathXmlApplicationContext(//
+        "com/eomcs/spring/ioc/ex01/a/application-context.xml");
 
     System.out.println("실행 완료!");
   }
--- bitcamp-java/src/main/java/com/eomcs/spring/ioc/ex01/b/Exam01.java
+++ bitcamp-java/src/main/java/com/eomcs/spring/ioc/ex01/b/Exam01.java
@@ -11,11 +11,12 @@ public class Exam01 {
     // => 자바 classpath가 아닌 다른 폴더에 설정파일이 있을 경우 사용한다.
     // => 설정 파일 경로를 지정할 때 파일 시스템 경로를 지정해야 한다.
     // => 단, URL 형식으로 지정해야 한다.
-    // => 예) file:// 설정파일경로
-    // => URL 형식에서는 파일 시스템을 가리킬 때 'file://' 접두어를 붙인다.
+    // => 예) file://설정파일경로
+    // => URL 형식에서는 파일 시스템을 가리킬 때 접두어 'file://'를 붙인다.
     //
-    ApplicationContext iocContainer2 = new FileSystemXmlApplicationContext(
-        "file://\\Users\\doll9\\git\\bitcamp-study\\bitcamp-java\\src\\main\\java\\com\\eomcs\\spring\\ioc\\ex01\\b\\application-context.xml");
+    ApplicationContext iocContainer = new FileSystemXmlApplicationContext(//
+        "file:///Users/eomjinyoung/git/eomcs-java/src/main/java/com/eomcs/spring/ioc/ex01/b/application-context.xml");
+
     System.out.println("실행 완료!");
   }
 }
